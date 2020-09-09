44e8f812b69fc612be14162aaf154f301817786d
Stephen Mehi <smehi@twistbioscience.com>
2020-09-09 14:20:41 -0700

    added vagrant install script

---
+++ VagrantInstall.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install virtualbox -y
+curl -O https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_x86_64.deb
+sudo apt install ./vagrant_2.2.10_x86_64.deb -y
+
+
