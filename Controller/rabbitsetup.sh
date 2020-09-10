bb37aa9853f2a9258adb7640318f6c4b255c4295
Stephen Mehi <smehi@twistbioscience.com>
2020-09-09 17:41:13 -0700

    first pass on bootstrap script

--- Controller/rabbitsetup.sh
+++
@@ -1,6 +0,0 @@
-cd
-cd /home/smehi/cluster-operator
-kubectl create -f config/namespace/base/namespace.yaml
-kubectl create -f config/crd/bases/rabbitmq.com_rabbitmqclusters.yaml
-kubectl -n rabbitmq-system create --kustomize config/rbac/
-kubectl -n rabbitmq-system create --kustomize config/manager/
