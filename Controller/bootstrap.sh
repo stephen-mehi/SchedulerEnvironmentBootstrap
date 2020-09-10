d40865501e8ab185acb123c6145b38327f4c389e
Stephen Mehi <smehi@twistbioscience.com>
2020-09-09 17:05:22 -0700

    updated bootstrap files

--- Controller/bootstrap.sh
+++ Controller/bootstrap.sh
@@ -1,6 +1,19 @@
-sudo apt-get update
-sudo apt-get install virtualbox -y
-curl -O https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_x86_64.deb
-sudo apt install ./vagrant_2.2.10_x86_64.deb -y
-vagrant --version
+apt-get update
 
+#PULL AND INSTALL KUBECTL 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+kubectl version –client
+
+#PULL AND INSTALL RANCHER RKE
+curl -s https://api.github.com/repos/rancher/rke/releases/v1.1.6 | grep download_url | grep amd64 | cut -d '"' -f 4 | wget -qi -
+chmod +x rke_linux-amd64
+mv rke_linux-amd64 /usr/local/bin/rke
+rke –version
+
+#PULL AND INSTALL HELM
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/v3.3.1/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+helm version
--- Node/bootstrap.sh
+++ Node/bootstrap.sh
@@ -1,6 +1,60 @@
-sudo apt-get update
-sudo apt-get install virtualbox -y
-curl -O https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_x86_64.deb
-sudo apt install ./vagrant_2.2.10_x86_64.deb -y
-vagrant --version
+
+apt-get update
+
+#MOUNT EXT4 FORMATTED DISK AT SPECIFIED DIR
+#USED BY KUBERNETES LOCAL PERSISTEN VOLS
+mount -t ext4 /kubevol /mnt/fast-disks/vol1
+
+#ENABLE KEY SYS MODULES 
+for module in br_netfilter ip6_udp_tunnel ip_set ip_set_hash_ip ip_set_hash_net iptable_filter iptable_nat iptable_mangle iptable_raw nf_conntrack_netlink nf_conntrack nf_conntrack_ipv4   nf_defrag_ipv4 nf_nat nf_nat_ipv4 nf_nat_masquerade_ipv4 nfnetlink udp_tunnel veth vxlan x_tables xt_addrtype xt_conntrack xt_comment xt_mark xt_multiport xt_nat xt_recent xt_set  xt_statistic xt_tcpudp;
+  do
+    if ! lsmod | grep -q $module; then
+      modprobe $module
+    fi;
+done
+
+#DISABLE SWAP DISK
+#THIS PREVENTS OS FROM DUMPING MEMORY OVERFLOW INTO HARD DISK
+swapoff -a
+
+#COMMENT OUT SWAP RECORD
+sed -i 's/^([^#].*?\sswap\s+.*)$/#\1/g' /etc/fstab
+
+#ADD NETWORK CONFIG FOR K8S
+tee -a /etc/sysctl.d/99-kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+#CONFIRM NO SWAP CONFIGURED
+sysctl --system
+free -h
+
+#PULL AND INSTALL DOCKER AND ADD CURRENT USER TO DOCKER GROUP
+curl https://releases.rancher.com/install-docker/19.03.sh | bash -
+systemctl enable --now docker
+docker version --format '{{.Server.Version}}'
+usermod -aG docker $USER
+id $USER
+
+#INSTALL MODULE USED FOR CONFIGURING PORTS
+apt-get install firewalld -y
+
+#OPEN CORRENT UDP/TCP PORTS
+for i in 22 80 443 179 5473 6443 8472 2376 8472 2379-2380 9099 10250 10251 10252 10254 30000-32767; do
+    firewall-cmd --add-port=${i}/tcp --permanent
+done
+firewall-cmd --reload
+
+for i in 8285 8472 4789 30000-32767; do
+   firewall-cmd --add-port=${i}/udp --permanent
+done
+
+#UPDATE SSH SETTINGS TO ALLOW TCP FORWARDING
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/#AllowTcpForwarding/AllowTcpForwarding/g' /etc/ssh/sshd_config
+
+systemctl restart ssh
+
 
