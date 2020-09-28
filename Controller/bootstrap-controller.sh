9ec906df148eb337f98eae1370f52bf81f95cdb0
Stephen Mehi <smehi@twistbioscience.com>
2020-09-27 21:30:08 -0700

    moved installation resources into folder. Added yaml manifests. removed test env folders

--- Controller/bootstrap-controller.sh
+++ Controller/bootstrap-controller.sh
@@ -60,194 +60,8 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 helm version
 
-#ADD NETWORK CONFIG FOR K8S
-tee cluster.yml <<EOF
-nodes:
-- address: 192.168.0.11
-  port: "22"
-  internal_address: 192.168.0.11
-  role:
-  - controlplane
-  - worker
-  - etcd
-  hostname_override: ""
-  user: vagrant
-  docker_socket: /var/run/docker.sock
-  ssh_key: ""
-  ssh_key_path: /home/vagrant/.ssh/id_rsa
-  ssh_cert: ""
-  ssh_cert_path: ""
-  labels: {}
-  taints: []
-services:
-  etcd:
-    image: ""
-    extra_args: {}
-    extra_binds: []
-    extra_env: []
-    win_extra_args: {}
-    win_extra_binds: []
-    win_extra_env: []
-    external_urls: []
-    ca_cert: ""
-    cert: ""
-    key: ""
-    path: ""
-    uid: 0
-    gid: 0
-    snapshot: null
-    retention: ""
-    creation: ""
-    backup_config: null
-  kube-api:
-    image: ""
-    extra_args: {}
-    extra_binds: []
-    extra_env: []
-    win_extra_args: {}
-    win_extra_binds: []
-    win_extra_env: []
-    service_cluster_ip_range: 10.43.0.0/16
-    service_node_port_range: ""
-    pod_security_policy: false
-    always_pull_images: false
-    secrets_encryption_config: null
-    audit_log: null
-    admission_configuration: null
-    event_rate_limit: null
-  kube-controller:
-    image: ""
-    extra_args: {}
-    extra_binds: []
-    extra_env: []
-    win_extra_args: {}
-    win_extra_binds: []
-    win_extra_env: []
-    cluster_cidr: 10.42.0.0/16
-    service_cluster_ip_range: 10.43.0.0/16
-  scheduler:
-    image: ""
-    extra_args: {}
-    extra_binds: []
-    extra_env: []
-    win_extra_args: {}
-    win_extra_binds: []
-    win_extra_env: []
-  kubelet:
-    image: ""
-    extra_args: {}
-    extra_binds: [/mnt/fast-disks:/mnt/fast-disks]
-    extra_env: []
-    win_extra_args: {}
-    win_extra_binds: []
-    win_extra_env: []
-    cluster_domain: cluster.local
-    infra_container_image: ""
-    cluster_dns_server: 10.43.0.10
-    fail_swap_on: false
-    generate_serving_certificate: false
-  kubeproxy:
-    image: ""
-    extra_args: {}
-    extra_binds: []
-    extra_env: []
-    win_extra_args: {}
-    win_extra_binds: []
-    win_extra_env: []
-network:
-  plugin: canal
-  options: {}
-  mtu: 0
-  node_selector: {}
-  update_strategy: null
-authentication:
-  strategy: x509
-  sans: []
-  webhook: null
-addons: ""
-addons_include: []
-system_images:
-  etcd: rancher/coreos-etcd:v3.4.3-rancher1
-  alpine: rancher/rke-tools:v0.1.64
-  nginx_proxy: rancher/rke-tools:v0.1.64
-  cert_downloader: rancher/rke-tools:v0.1.64
-  kubernetes_services_sidecar: rancher/rke-tools:v0.1.64
-  kubedns: rancher/k8s-dns-kube-dns:1.15.2
-  dnsmasq: rancher/k8s-dns-dnsmasq-nanny:1.15.2
-  kubedns_sidecar: rancher/k8s-dns-sidecar:1.15.2
-  kubedns_autoscaler: rancher/cluster-proportional-autoscaler:1.7.1
-  coredns: rancher/coredns-coredns:1.6.9
-  coredns_autoscaler: rancher/cluster-proportional-autoscaler:1.7.1
-  nodelocal: rancher/k8s-dns-node-cache:1.15.7
-  kubernetes: rancher/hyperkube:v1.18.8-rancher1
-  flannel: rancher/coreos-flannel:v0.12.0
-  flannel_cni: rancher/flannel-cni:v0.3.0-rancher6
-  calico_node: rancher/calico-node:v3.13.4
-  calico_cni: rancher/calico-cni:v3.13.4
-  calico_controllers: rancher/calico-kube-controllers:v3.13.4
-  calico_ctl: rancher/calico-ctl:v3.13.4
-  calico_flexvol: rancher/calico-pod2daemon-flexvol:v3.13.4
-  canal_node: rancher/calico-node:v3.13.4
-  canal_cni: rancher/calico-cni:v3.13.4
-  canal_flannel: rancher/coreos-flannel:v0.12.0
-  canal_flexvol: rancher/calico-pod2daemon-flexvol:v3.13.4
-  weave_node: weaveworks/weave-kube:2.6.4
-  weave_cni: weaveworks/weave-npc:2.6.4
-  pod_infra_container: rancher/pause:3.1
-  ingress: rancher/nginx-ingress-controller:nginx-0.32.0-rancher1
-  ingress_backend: rancher/nginx-ingress-controller-defaultbackend:1.5-rancher1
-  metrics_server: rancher/metrics-server:v0.3.6
-  windows_pod_infra_container: rancher/kubelet-pause:v0.1.4
-ssh_key_path: /home/vagrant/.ssh/id_rsa
-ssh_cert_path: ""
-ssh_agent_auth: false
-authorization:
-  mode: rbac
-  options: {}
-ignore_docker_version: null
-kubernetes_version: ""
-private_registries: []
-ingress:
-  provider: ""
-  options: {}
-  node_selector: {}
-  extra_args: {}
-  dns_policy: ""
-  extra_envs: []
-  extra_volumes: []
-  extra_volume_mounts: []
-  update_strategy: null
-cluster_name: ""
-cloud_provider:
-  name: ""
-prefix_path: ""
-win_prefix_path: ""
-addon_job_timeout: 0
-bastion_host:
-  address: ""
-  port: ""
-  user: ""
-  ssh_key: ""
-  ssh_key_path: ""
-  ssh_cert: ""
-  ssh_cert_path: ""
-monitoring:
-  provider: ""
-  options: {}
-  node_selector: {}
-  update_strategy: null
-  replicas: null
-restore:
-  restore: false
-  snapshot_name: ""
-dns: null
-EOF
-
-
-#REPLACE IP AND USER IN RKE CLUSTER YAML
-#sed -i 's/IP_REPLACE/'"$0"'/g' cluster.yml
-#sed -i 's/USER_REPLACE/'"$1"'/g' cluster.yml
-
+#GET RKE MANIFEST
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/cluster.yaml
 rke up
 
 #SET KUBCTL CONFIG ENV VAR
@@ -291,231 +105,67 @@ kubectl create -f /root/sig-storage-local-static-provisioner/provisioner_generat
 
 cd
 
-tee local-storage.yaml <<EOF
-apiVersion: storage.k8s.io/v1
-kind: StorageClass
-metadata:
-  name: fast-disks
-provisioner: kubernetes.io/no-provisioner
-volumeBindingMode: WaitForFirstConsumer
-EOF
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/local-storage-class.yaml
 
 kubectl create namespace rabbitmq
-kubectl apply -f local-storage.yaml --namespace rabbitmq
+kubectl apply -f local-storage-class.yaml --namespace rabbitmq
 
-tee rabbitmqdef.yaml <<EOF
-apiVersion: rabbitmq.com/v1beta1
-kind: RabbitmqCluster
-metadata:
-  name: rabbitmqdef
-spec:
-  persistence:
-    storageClassName: fast-disks
-    storage: 40Gi
-  replicas: 1 
-  resources:
-    requests:
-      cpu: 1000m
-      memory: 2Gi
-    limits:
-      cpu: 1000m
-      memory: 2Gi  
-EOF
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/rabbitmq-cluster.yaml
 
 #APPLY RABBITMQ CLUSTER YAML
-kubectl apply -f rabbitmqdef.yaml --namespace rabbitmq
+kubectl apply -f rabbitmq-cluster.yaml --namespace rabbitmq
 
 kubectl create namespace cockroachdb
 kubectl config set-context $(kubectl config current-context) --namespace=cockroachdb
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/cockroachdb-cluster.yaml
 
-tee cockroachdb-statefulset.yaml <<EOF
-apiVersion: v1
-kind: Service
-metadata:
-  # This service is meant to be used by clients of the database. It exposes a ClusterIP that will
-  # automatically load balance connections to the different database pods.
-  name: cockroachdb-public
-  labels:
-    app: cockroachdb
-spec:
-  ports:
-  # The main port, served by gRPC, serves Postgres-flavor SQL, internode
-  # traffic and the cli.
-  - port: 26257
-    targetPort: 26257
-    name: grpc
-  # The secondary port serves the UI as well as health and debug endpoints.
-  - port: 8080
-    targetPort: 8080
-    name: http
-  selector:
-    app: cockroachdb
----
-apiVersion: v1
-kind: Service
-metadata:
-  # This service only exists to create DNS entries for each pod in the stateful
-  # set such that they can resolve each other's IP addresses. It does not
-  # create a load-balanced ClusterIP and should not be used directly by clients
-  # in most circumstances.
-  name: cockroachdb
-  labels:
-    app: cockroachdb
-  annotations:
-    # Use this annotation in addition to the actual publishNotReadyAddresses
-    # field below because the annotation will stop being respected soon but the
-    # field is broken in some versions of Kubernetes:
-    # https://github.com/kubernetes/kubernetes/issues/58662
-    service.alpha.kubernetes.io/tolerate-unready-endpoints: "true"
-    # Enable automatic monitoring of all instances when Prometheus is running in the cluster.
-    prometheus.io/scrape: "true"
-    prometheus.io/path: "_status/vars"
-    prometheus.io/port: "8080"
-spec:
-  ports:
-  - port: 26257
-    targetPort: 26257
-    name: grpc
-  - port: 8080
-    targetPort: 8080
-    name: http
-  # We want all pods in the StatefulSet to have their addresses published for
-  # the sake of the other CockroachDB pods even before they're ready, since they
-  # have to be able to talk to each other in order to become ready.
-  publishNotReadyAddresses: true
-  clusterIP: None
-  selector:
-    app: cockroachdb
----
-apiVersion: policy/v1beta1
-kind: PodDisruptionBudget
-metadata:
-  name: cockroachdb-budget
-  labels:
-    app: cockroachdb
-spec:
-  selector:
-    matchLabels:
-      app: cockroachdb
-  maxUnavailable: 1
----
-apiVersion: apps/v1
-kind: StatefulSet
-metadata:
-  name: cockroachdb
-spec:
-  serviceName: "cockroachdb"
-  replicas: 1
-  selector:
-    matchLabels:
-      app: cockroachdb
-  template:
-    metadata:
-      labels:
-        app: cockroachdb
-    spec:
-      affinity:
-        podAntiAffinity:
-          preferredDuringSchedulingIgnoredDuringExecution:
-          - weight: 100
-            podAffinityTerm:
-              labelSelector:
-                matchExpressions:
-                - key: app
-                  operator: In
-                  values:
-                  - cockroachdb
-              topologyKey: kubernetes.io/hostname
-      containers:
-      - name: cockroachdb
-        image: cockroachdb/cockroach:v19.2.5
-        imagePullPolicy: IfNotPresent
-        # TODO: Change these to appropriate values for the hardware that you're running. You can see
-        # the amount of allocatable resources on each of your Kubernetes nodes by running:
-        #   kubectl describe nodes
-        resources:
-          requests:
-            cpu: "2"
-            memory: "2Gi"
-        ports:
-        - containerPort: 26257
-          name: grpc
-        - containerPort: 8080
-          name: http
-        livenessProbe:
-          httpGet:
-            path: "/health"
-            port: http
-          initialDelaySeconds: 30
-          periodSeconds: 5
-        readinessProbe:
-          httpGet:
-            path: "/health?ready=1"
-            port: http
-          initialDelaySeconds: 10
-          periodSeconds: 5
-          failureThreshold: 2
-        volumeMounts:
-        - name: datadir
-          mountPath: /cockroach/cockroach-data
-        env:
-        - name: COCKROACH_CHANNEL
-          value: kubernetes-insecure
-        command:
-          - "/bin/bash"
-          - "-ecx"
-          # The use of qualified `hostname -f` is crucial:
-          # Other nodes aren't able to look up the unqualified hostname.
-          - "exec /cockroach/cockroach start --logtostderr --insecure --advertise-host $(hostname -f) --http-addr 0.0.0.0 --join cockroachdb-0.cockroachdb,cockroachdb-1.cockroachdb,cockroachdb-2.cockroachdb --cache 25% --max-sql-memory 25%"
-      # No pre-stop hook is required, a SIGTERM plus some time is all that's
-      # needed for graceful shutdown of a node.
-      terminationGracePeriodSeconds: 60
-      #volumes:
-      #- name: datadir
-      #  persistentVolumeClaim:
-      #    claimName: datadir
-  podManagementPolicy: Parallel
-  updateStrategy:
-    type: RollingUpdate
-  volumeClaimTemplates:
-  - metadata:
-      name: datadir
-    spec:
-      storageClassName: fast-disks
-      accessModes:
-        - "ReadWriteOnce"
-      resources:
-        requests:
-          storage: 40Gi
-EOF
+kubectl create -f cockroachdb-cluster.yaml
 
-tee cluster-init.yaml <<EOF
-apiVersion: batch/v1
-kind: Job
-metadata:
-  name: cluster-init
-  labels:
-    app: cockroachdb
-spec:
-  template:
-    spec:
-      containers:
-      - name: cluster-init
-        image: cockroachdb/cockroach:v19.2.5
-        imagePullPolicy: IfNotPresent
-        command:
-          - "/cockroach/cockroach"
-          - "init"
-          - "--insecure"
-          - "--host=cockroachdb-0.cockroachdb"
-      restartPolicy: OnFailure
-EOF
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/cockroachdb-cluster-init.yaml
 
-kubectl create -f cockroachdb-statefulset.yaml
-kubectl create -f cluster-init.yaml
+kubectl create -f cockroachdb-cluster-init.yaml
 
 kubectl config set-context $(kubectl config current-context) --namespace=default
+
+git clone https://github.com/prometheus-operator/kube-prometheus.git
+cd kube-prometheus
+git checkout v0.6.0
+kubectl create -f manifests/setup
+until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+kubectl create -f manifests/
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/prometheus-role.yaml
+
+#ESTABLISH ELEVATED PROMETHEUS PERMISSIONS 
+kubectl apply -f prometheus-role.yaml -n monitoring
+
+#CONFIGURE POD MONITORING FOR PROMETHEUS TO START SCRAPING RMQ METRICs
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/prometheus-monitors.yaml
+
+kubectl apply -f prometheus-monitors.yaml -n monitoring
+
+#EXPOSE PROMETHEUS THROUGH INGRESS
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/stephen-mehi/SchedulerEnvironmentBootstrap/v0.0.1/manifests/ingress.yaml
+
+kubectl apply -f ingress.yaml -n monitoring
+
+apt-get install jq
+
+grafana_host="http://scheduler.com"
+grafana_cred="admin:admin"
+grafana_datasource="prometheus"
+ds=(11465);
+for d in "${ds[@]}"; do
+  echo -n "Processing $d: "
+  j=$(curl -k -u "$grafana_cred" $grafana_host/api/gnet/dashboards/$d | jq .json)
+  curl -s -k -u "$grafana_cred" -XPOST -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -d "{\"dashboard\":$j,\"overwrite\":true, \
+        \"inputs\":[{\"name\":\"prometheus\",\"type\":\"datasource\", \
+        \"pluginId\":\"prometheus\",\"value\":\"$grafana_datasource\"}]}" \
+    $grafana_host/api/dashboards/import; echo ""
+done
 
 
 #UPDATE SSH SETTINGS TO ALLOW TCP FORWARDING
--- Tools/TestEnv/bootstrap-ssh-2.sh
+++
@@ -1,8 +0,0 @@
-#sed -i 's/#   StrictHostKeyChecking ask/    StrictHostKeyChecking no/g' /etc/ssh/ssh_config
-
-ssh-keygen -b 2048 -t rsa -f "/home/vagrant/.ssh/id_rsa" -q -N "" <<< ""$'\n'"y" 2>&1
-sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa vagrant@192.168.0.11
-
-#ssh-copy-id -f -i /home/vagrant/.ssh/id_rsa vagrant@192.168.0.11 <<< ""$'\n'"vagrant" 2>&1
-#sshpass -p vagrant ssh vagrant@192.168.0.11 'cat .ssh/id_rsa.pub' >> /home/vagrant/host-ids.pub
-
